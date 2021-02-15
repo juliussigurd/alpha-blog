d1bad86d1472763461e5f069b20e31ee5c320366
julius heilmann <juliussigurd@live.dk>
2021-02-15 19:29:06 +0100

    added display of number of articles associate ith each category in the categories index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,12 +15,13 @@ class CategoriesController < ApplicationController
         end
     end
 
-    def show
-        @category = Category.find(params[:id])
-    end
-
     def index
         @categories = Category.paginate(page: params[:page], per_page: 5)
+    end
+    
+    def show
+        @category = Category.find(params[:id])
+        @articles = @category.articles.paginate(page: params[:page], per_page: 5)
     end
 
     private 
